cc1adad2c718bc6a1583127bdeff2c18cef9bd95
Yurij Mrozek <69200681+yurijmrozek@users.noreply.github.com>
2021-03-21 06:51:57 +0200

    ok

--- src/main/java/world/ucode/UploadServlet.java
+++ src/main/java/world/ucode/UploadServlet.java
@@ -8,13 +8,20 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "upload-servlet", value = "/upload-servlet")
 public class UploadServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("fname");
+        String size = request.getParameter("fsize");
+        String format = request.getParameter("fformat");
+        Part file = request.getPart("file");
+        String range = request.getParameter("range");
 
+        response.getOutputStream().print("Name: " + name
+                    + "\nSize: " + size
+                    + "\nFormat: " + format
+                    + "\nFile: " + file
+                    + "\nRange: " + range);
     }
+
 }
--- src/main/webapp/js/fetch.js
+++ src/main/webapp/js/fetch.js
@@ -1,6 +1,87 @@
-﻿function upload_image_js() {
-    console.log("im in js")
-    fetch("http://localhost:8081/upload-servlet", {
-        method: 'POST'
-    })
+﻿let file;
+let range;
+let fformat;
+let fsize;
+
+async function pixelate_post() {
+    const url = "http://localhost:8081/upload-servlet";
+    const form_data = new FormData();
+
+    if (file && check_format()) {
+        form_data.append('file', file);
+        form_data.set('fname', file.name);
+        form_data.set('fformat', fformat);
+        form_data.set('fsize', fsize);
+        form_data.set('range', range.value);
+
+        try {
+            let resp = await fetch(url, {
+                method: 'POST',
+                body: form_data,
+                enctype: 'multipart/form-data'
+            });
+            const data = await resp.text();
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+function check_format() {
+    fformat = file.name.substr(file.name.lastIndexOf('.', file.name.length) + 1);
+    fformat = fformat.toLowerCase();
+    const document_error = document.getElementById('error-text');
+    if (fformat === 'png'
+        || fformat === 'bmp'
+        || fformat === 'jpeg'
+        || fformat === 'jpg') {
+        if (fsize > 1024) {
+            document_error.textContent = "";
+            return true;
+        } else {
+            document_error.textContent = "File corrupted.";
+            return false;
+        }
+    } else {
+        document_error.textContent = "Wrong file format. Choose other.";
+        return false;
+    }
+}
+
+function preview_file() {
+    document.getElementById('error-text').textContent = "";
+    file = document.querySelector('input[type=file]').files[0];
+    if (file)
+        fsize = document.querySelector('input[type=file]').files[0].size;
+    const preview = document.querySelector('img');
+    const reader = new FileReader();
+
+    reader.onloadend = function() { preview.src = reader.result; }
+    if (file && check_format())
+        reader.readAsDataURL(file);
+    else
+        preview.src = "";
+}
+
+function range_changed(from) {
+    range = document.getElementById("range");
+    const num = document.getElementById("range-num");
+
+    if (from === "range") {
+        num.value = range.value;
+
+        range.oninput = function() {
+            num.innerHTML = this.value;
+        }
+        pixelate_post();
+    } else {
+        if (num.value <= 100 && num.value >= 0) {
+            range.value = num.value;
+            num.oninput = function() {
+                range.innerHTML = this.value;
+            }
+            pixelate_post();
+        }
+    }
 }
